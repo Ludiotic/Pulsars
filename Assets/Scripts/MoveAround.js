2dc88fd101b8a3ee71a037d995218e0109a6e7a5
Gaffen <tonedeafmessiah@gmail.com>
2012-11-13 00:24:43 +0000

    mouse controls added

--- Assets/Scripts/MoveAround.js
+++ Assets/Scripts/MoveAround.js
@@ -8,13 +8,13 @@ function Update () {
 	
 	// Rotate around Y - Axis
 	
-	transform.Rotate(Input.GetAxis ("Vertical") * rotateSpeed, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+	transform.Rotate(-Input.GetAxis ("Mouse Y") * rotateSpeed, Input.GetAxis ("Horizontal") * rotateSpeed, -Input.GetAxis ("Mouse X") * rotateSpeed);
 	
 
 	//Move forward / backward
-	//var forward = transform.TransformDirection(Vector3.forward);
-	//var curSpeed = speed * Input.GetAxis ("Vertical");
-	//controller.SimpleMove(forward * curSpeed);
+	var forward = transform.TransformDirection(Vector3.forward);
+	var curSpeed = speed * Input.GetAxis ("Vertical");
+	controller.SimpleMove(forward * curSpeed);
 	
 }
 
